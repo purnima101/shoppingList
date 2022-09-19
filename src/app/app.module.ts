bcaacbc0ccc08e6b066d67a429f725593822046d
purnima101 <purnimaagarwal2013@gmail.com>
2022-09-19 18:28:07 +0530

    hii

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './components/header/header.component';
+import { RecepiesComponent } from './components/recepies/recepies.component';
+import { RecepieListComponent } from './components/recepies/recepie-list/recepie-list.component';
+import { RecepieDetailsComponent } from './components/recepies/recepie-details/recepie-details.component';
+import { RecepieItemComponent } from './components/recepies/recepie-list/recepie-item/recepie-item.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './components/shopping-list/shopping-edit/shopping-edit.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecepiesComponent,
+    RecepieListComponent,
+    RecepieDetailsComponent,
+    RecepieItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+ 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
